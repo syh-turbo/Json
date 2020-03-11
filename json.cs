308abc1d275666669fcda89c8aa496f529725276
syh_turbo <33369368+syh-turbo@users.noreply.github.com>
2020-03-11 16:47:08 +0800

    Add files via upload

---
+++ json.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoLearning.VideoLearningServer
+{
+    public class json
+    {
+        static void Main(string[] args)
+
+        {
+
+            ShowData sd = new ShowData()
+
+            {
+
+                text1 = "百度",
+
+                text2 = "baidu",
+
+                text3 = "百度经验"
+
+            };
+
+            string jsonStr = JsonConvert.SerializeObject(sd);
+
+            Console.WriteLine(jsonStr);
+
+        }
+
+    }
+
+    public class ShowData
+
+    {
+
+        public string text1 { get; set; }
+
+        public string text2 { get; set; }
+
+        public string text3 { get; set; }
+
+    }
+}
